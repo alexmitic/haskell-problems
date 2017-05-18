56147c2691c8b0a536393668bf4662a1b547e069
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-18 23:37:50 +0200

    rovarsprak finished

--- uppvärmning.hs
+++ uppvärmning.hs
@@ -1,4 +1,5 @@
 module F1 where
+        
 -- Fibonacci
 fib :: Integer -> Integer
 fib 0 = 0
@@ -6,8 +7,14 @@ fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 -- Rövarspråk
-rovarsprak s = s
+rovarsprak :: String -> String
+rovarsprak [] = []
+rovarsprak (x:xs) --Separate head and tail
+        | x `notElem` "aeiouy"   = x:'o':x: rovarsprak xs -- If tail not vocal
+        | otherwise              = x: rovarsprak xs
 
+-- Omvändt rövarspråk
 karpsravor s = s
+
 medellangd s = 1.0
 skyffla s = s
