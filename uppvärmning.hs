6e3c89093479d50a59ef3f5358e73aee9cdfb188
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-18 21:55:02 +0200

    Finished Fibonacchi

---
+++ uppvärmning.hs
@@ -0,0 +1,13 @@
+module F1 where
+-- Fibonacci
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+-- Rövarspråk
+rovarsprak s = s
+
+karpsravor s = s
+medellangd s = 1.0
+skyffla s = s
