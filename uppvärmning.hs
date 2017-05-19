8174b29d1172daae9da49573b0df91a9d18b3855
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-19 17:32:29 +0200

    Rovarsprak now fully finished

--- uppvärmning.hs
+++ uppvärmning.hs
@@ -10,11 +10,16 @@ fib n = fib (n - 1) + fib (n - 2)
 rovarsprak :: String -> String
 rovarsprak [] = []
 rovarsprak (x:xs) --Separate head and tail
-        | x `notElem` "aeiouy"   = x:'o':x: rovarsprak xs -- If tail not vocal
+        | x `notElem` "aeiouy"   = x:'o':x: rovarsprak xs -- If head not vocal
         | otherwise              = x: rovarsprak xs
 
 -- Omvändt rövarspråk
-karpsravor s = s
+karpsravor :: String -> String
+karpsravor [] = []
+karpsravor (x:xs) --Separate head and tail
+        | x `notElem` "aeiouy"   = x: karpsravor (tail (tail xs)) -- If head not vocal
+        | otherwise              = x: karpsravor xs
+
 
 medellangd s = 1.0
 skyffla s = s
