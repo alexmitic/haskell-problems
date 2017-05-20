ad6650ae8fb2106762c9865fa7bf9b461ca0b484
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-20 23:11:43 +0200

    Type created and added DNA-check

---
+++ LabbF2/kth.progp.f2.hs
@@ -0,0 +1,9 @@
+module F2 where
+
+data MolSeq = MolSeq String String String deriving (Show)
+
+checkIfDNA :: String -> Bool
+checkIfDNA [] = True
+checkIfDNA (x:xs)
+        | x `elem` "ACGT" = checkIfDNA xs -- If head is DNA check rest
+        | otherwise = False -- If not DNA
