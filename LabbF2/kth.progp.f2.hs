2fda8ef3cee13059ba252c611fb3b12dd9970052
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-20 23:19:16 +0200

    string2seq implemented

--- LabbF2/kth.progp.f2.hs
+++ LabbF2/kth.progp.f2.hs
@@ -7,3 +7,8 @@ checkIfDNA [] = True
 checkIfDNA (x:xs)
         | x `elem` "ACGT" = checkIfDNA xs -- If head is DNA check rest
         | otherwise = False -- If not DNA
+
+string2seq :: String -> String -> MolSeq
+string2seq n s
+        | checkIfDNA s = MolSeq n s "DNA" -- If sekvens is DNA
+        | otherwise = MolSeq n s "PROTEIN" -- Protein
