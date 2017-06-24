c9491b874e0aafb3521cdc95fd36bacaaf4a62cf
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-06-25 00:07:05 +0200

    Finished L1 and passed kattis, preformance optimising needed badly

--- L1/uppvarmning.pl
+++ L1/uppvarmning.pl
@@ -22,29 +22,60 @@ fib(A, B, N, F) :-
 rovarsprak([], []) :- !. 
 
 rovarsprak([Head|TailText], [Head|TailRov]) :-  % If first is the same
-        member(Head, [97, 101, 111, 117, 121]), % And is a vokal
+        member(Head, [97, 101, 105, 111, 117, 121]), !, % And is a vokal
         rovarsprak(TailText, TailRov).          % Check rest 
 
 rovarsprak([Head|TailText], [Head, 111, Head | TailRov]) :- % If first in text is konsonant
+        !,
         rovarsprak(TailText, TailRov).
                         
 
 % Medellangd
 
-medellangd(List, AvgLength) :- sum(List, Chars, 0, Words, 0), AvgLength is Chars / Words, !. 
+medellangd(List, AvgLength) :- sum(List, Letters, Words), AvgLength is Letters / Words, !. 
     
 % Helper that calculates number of words and number of letters
+% Basecases
+sum([], 0, 0). % Empty list equals zero words and letters
 
-sum([], Chars, Chars, Words, Words). % If calculated num words and letter equals the given input 
+sum([Head|[]], 1, 1) :- % If list only contains one letter it also has one word
+        char_code(Letter, Head),
+        char_type(Letter, alpha).
 
-sum([Head|Tail], Chars, CharCounter, Words, WordCounter) :- % If letter, increment CharCounter
+sum([Head, Second|Tail], Letters, Words) :- % If second is not letter and head is letter we have one word and one letter
+        char_code(LetterSecond, Second),
+        not(char_type(LetterSecond, alpha)),
+        char_code(LetterFirst, Head),
+        char_type(LetterFirst, alpha),
+        sum(Tail, Letters1, Words1),
+        Letters is Letters1 + 1,
+        Words is Words1 + 1.
+
+sum([Head|Tail], Letters, Words) :- % If head letter, words stay the same, letter increments
         char_code(Letter, Head),
         char_type(Letter, alpha),
-        IncCh is CharCounter + 1, 
-        sum(Tail, Chars, IncCh, Words, WordCounter).
+        sum(Tail, Letters1, Words1),
+        Letters is Letters1 + 1,
+        Words is Words1. 
 
-sum([Head|Tail], Chars, CharCounter, Words, WordCounter) :- % If not letter, increment WordCounter
-        char_code(Letter, Head),
-        not(char_type(Letter, alpha)),
-        IncWords is WordCounter + 1, 
-        sum(Tail, Chars, CharCounter, Words, IncWords).
+sum([_|Tail], Letters, Words) :- % If none of the above continue with next part of list
+        sum(Tail, Letters, Words).
+
+% Skyffla
+
+% Basecases
+skyffla([], []).
+
+skyffla([Head|Tail], Skyfflad) :-
+        everyOther([Head|Tail], Result), % Get everyother 
+        everyOther(Tail, Rest), % Get what is remaining 
+        skyffla(Rest, RqRes), % Recursivly skyffla the remiander
+        append(Result, RqRes, Skyfflad). % Put together lists
+
+
+everyOther([], []) :- !.
+
+everyOther([Head|[]], [Head|[]]) :- !.
+
+everyOther([Head, _| Tail], [Head|TailFinal]) :-
+        everyOther(Tail, TailFinal).
