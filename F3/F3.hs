420cbe952b9ce175e6bd98149ec029d76cf495f7
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-07-12 19:01:34 +0200

    Added function that calculates totaldistance

--- F3/F3.hs
+++ F3/F3.hs
@@ -7,9 +7,9 @@ main = do
     content <- getContents
     length content `seq` return ()
     let dist =  distMa (mols (words content))
+    let totaldistanses = totalDist (mols (words content)) (mols (words content))
     print dist
-
-
+    print totaldistanses
 
 mols :: [String] -> [MolSeq]
 mols [] = []
@@ -18,3 +18,10 @@ mols (x:xs) = string2seq x (head xs) : mols (tail xs)
 distMa :: [MolSeq] -> [(String, String, Double)]
 distMa [] = []
 distMa list = [(seqName (head l), seqName y, abs(seqDistance (head l) y)) | l <- tails list, y <- l]
+
+totalDist :: [MolSeq] -> [MolSeq] -> [Double]
+totalDist [] _ = []
+totalDist (x:xs) list = totalDistOneRow x list : totalDist xs list 
+
+totalDistOneRow :: MolSeq -> [MolSeq] -> Double
+totalDistOneRow n list = sum (map (\x -> abs(seqDistance n x)) list)
