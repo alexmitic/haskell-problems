628a4b520c77ce2649ed67f286fe0858a1779651
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-10-30 15:39:29 +0100

    Started merging soloutions

--- S1/s1.py
+++ S1/s1.py
@@ -8,22 +8,13 @@ def hidden1(subString):
     return subString
 
 def hidden2(subString):
-    # First match any characters that are not subString
-    # After those matches, match one or more of the first letter 
-    regex = "[^" + subString + "]*" + "[" + subString[0:1] + "]+"                                                               
-    n = 1
+    temp = []
 
-    # Now follow the pattern where you match one or more that arent the second charcter from subString
-    # Then match one or more that are the second
-    # And so on...
-    for i in range(2 * (len(subString) - 1)):
-        if (i & 1) == 0:
-            regex = regex + "[^" + subString[n:n + 1] + "]*"
-        else:
-            regex = regex + "[" + subString[n:n + 1] + "]+"
-            n = n + 1
-    
-    return regex
+    for n in subString: # For all letter is given string
+        temp.append("[^" + n + "]*") # All that is not n
+        temp.append("[" + n + "]") # One of n
+
+    return ''.join(temp)
 
 # First check if a number. Negativ numbers are allowd
 # Then check for zero or more operators followd by numbers  
@@ -38,5 +29,6 @@ def parentheses():
 # Check if zero or more non-ascending numbers 
 # Followed by three numbers in ascending order 
 # And ending with zero or more non-ascending numbers
+# 
 def sorted3():
     return "^[\d]*(?:01[2-9]|[0-1]2[3-9]|[0-2]3[4-9]|[0-3]4[5-9]|[0-4]5[6-9]|[0-5]6[7-9]|[0-6]7[8-9]|[0-7]89)[\d]*$"
